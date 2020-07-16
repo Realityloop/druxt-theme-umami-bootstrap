814b8795288951086ce3e0c9e9b125a37f122b67
Stuart Clark <stuart@realityloop.com>
2020-07-16 17:41:41 +1000

    [#4] Updated recipe and field_difficulty.

--- src/components/fields/DruxtFieldListDefaultFieldDifficulty.vue
+++ src/components/fields/DruxtFieldListDefaultFieldDifficulty.vue
@@ -1,10 +1,15 @@
 <template>
-  <component :is="wrapper.component" v-bind="wrapper.props">
-    <b-icon icon="puzzle" font-scale="2"></b-icon>
+  <component v-if="context.icon" :is="wrapper.component" v-bind="wrapper.props">
+    <b-icon-puzzle font-scale="2" />
+
     <p class="mt-2">
       <span v-if="$slots['label-above']">{{ schema.label.text }}<br /></span>
       {{ items[0] }}
     </p>
+  </component>
+
+  <component v-else :is="wrapper.component" v-bind="wrapper.props">
+    <b-badge pill :variant="variant">{{ items[0] }}</b-badge>
   </component>
 </template>
 
@@ -17,9 +22,20 @@ export default {
 
   mixins: [DruxtFieldMixin],
 
-  components: {
-    BIcon,
-    BIconPuzzle,
+  components: { BIconPuzzle },
+
+  computed: {
+    variant() {
+      switch (this.items[0]) {
+        case 'easy':
+          return 'success'
+        case 'medium':
+          return 'warning'
+        case 'hard':
+          return 'danger'
+      }
+      return false
+    },
   },
 }
 </script>
